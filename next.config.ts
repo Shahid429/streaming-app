eab2d63ef762e18b7553500322deefd8104d69a1
shahid429 <shahidmir429@gmail.com>
2025-01-17 16:24:36 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "export", // Enables static export
 };
 
 export default nextConfig;
