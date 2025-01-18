86f971c9b2ee7e1d63d2a713fdc554025cdd8fa0
shahid429 <shahidmir429@gmail.com>
2025-01-18 15:48:49 +0530

    Your commit message here

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,45 +1,23 @@
-"use client"; // Mark this file as a client component
-
-import { useEffect, useRef, useState } from 'react';
+import React from 'react';
+import ReactPlayer from 'react-player';
 
 const VideoPlayer = ({ m3u8Url }) => {
-  const videoRef = useRef(null);
-  const [Hls, setHls] = useState(null);
-
-  useEffect(() => {
-    // Dynamically import Hls.js on the client-side
-    if (typeof window !== 'undefined') {
-      import('hls.js').then((module) => {
-        setHls(() => module.default); // Set Hls as a function
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (Hls && m3u8Url) {
-      const hls = new Hls(); // Instantiate Hls with 'new'
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(videoRef.current);
-
-      hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        console.log('Manifest parsed, the video can now be played!');
-      });
-
-      hls.on(Hls.Events.ERROR, function (event, data) {
-        if (data.fatal) {
-          console.error('HLS.js error:', data);
-        }
-      });
-
-      return () => {
-        hls.destroy();
-      };
-    }
-  }, [Hls, m3u8Url]);
-
   return (
     <div className="video-container">
-      <video ref={videoRef} controls />
+      <ReactPlayer
+        url={m3u8Url}
+        controls={true}
+        width="100%"
+        height="100%"
+        playing={true}
+        config={{
+          file: {
+            attributes: {
+              crossOrigin: 'anonymous', // Necessary for cross-origin requests
+            },
+          },
+        }}
+      />
     </div>
   );
 };
